b7ac8cfb4036205aa69b27ffb52598bef3338f93
Wafo Tokam Joël <129383095+Watojo78@users.noreply.github.com>
2025-04-03 13:51:05 -0400

    feat: mark app component as standalone for improved modularity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,9 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'fleet-root',
   imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'flowfleet';
